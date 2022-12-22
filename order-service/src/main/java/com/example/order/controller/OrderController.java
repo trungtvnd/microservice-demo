b075b60f042f2a4807016ee03a0a0bf2e0c67a89
trungtvnd <tranviettrung20011992@gmail.com>
2022-12-22 22:49:59 +0700

    add

--- order-service/src/main/java/com/example/order/controller/OrderController.java
+++ order-service/src/main/java/com/example/order/controller/OrderController.java
@@ -2,9 +2,12 @@ package com.example.order.controller;
 
 import com.example.order.dto.OrderRequest;
 import com.example.order.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,13 +18,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) throws IllegalAccessException {
         orderService.placeOrder(orderRequest);
 
         return "Order Create successfully";
     }
 
-//    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-//        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
-//    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
+    }
 }
