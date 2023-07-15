7a94192307e265606a5dd89e81d62b107f8a0aab
trungtvnd <tranviettrung20011992@gmail.com>
2023-07-15 21:35:50 +0700

    test jenkens heheheeh

--- order-service/src/main/java/com/example/order/controller/EtcController.java
+++ order-service/src/main/java/com/example/order/controller/EtcController.java
@@ -1,10 +1,12 @@
 package com.example.order.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test-jenkins")
+@CrossOrigin("*")
 public class EtcController {
 
 }
