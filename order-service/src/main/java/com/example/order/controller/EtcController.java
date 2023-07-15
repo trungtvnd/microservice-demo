7e5ab962125be1ec1d5aed8c73ad1e0780c9a850
trungtvnd <tranviettrung20011992@gmail.com>
2023-07-15 21:32:15 +0700

    test jenkens

--- order-service/src/main/java/com/example/order/controller/EtcController.java
+++ order-service/src/main/java/com/example/order/controller/EtcController.java
@@ -1,7 +1,10 @@
 package com.example.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("test-jenkins")
 public class EtcController {
+
 }
